db1fb48a767a83ef63c7baac496f44fe9bebdda5
Armando <armando.josue.og@gmail.com>
2019-12-14 15:44:26 +0900

    no muvjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,37 +1,57 @@
+//@flow
 import {BoxGeometry, Mesh, MeshNormalMaterial, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
 
-let camera, scene, renderer;
-let geometry, material, mesh;
+const initialState = {
+  camera: null,
+  scene: null,
+  renderer: null,
+  geometry: null,
+  material: null,
+  mesh: null,
+};
+/*::
+type State = typeof initialState
+ */
 
-init();
-animate();
+const init = state => {
 
-function init() {
+  state.camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  state.camera.position.z = 1;
 
-  camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-  camera.position.z = 1;
+  state.scene = new Scene();
 
-  scene = new Scene();
+  state.geometry = new BoxGeometry(0.2, 0.2, 0.2);
+  state.material = new MeshNormalMaterial();
 
-  geometry = new BoxGeometry( 0.2, 0.2, 0.2 );
-  material = new MeshNormalMaterial();
+  state.mesh = new Mesh(state.geometry, state.material);
+  state.scene.add(state.mesh);
 
-  mesh = new Mesh( geometry, material );
-  scene.add( mesh );
+  state.renderer = new WebGLRenderer({antialias: true});
+  state.renderer.setSize(window.innerWidth, window.innerHeight);
+  if (document.body && state.renderer) {
+    document.body.appendChild(state.renderer.domElement);
+  }
 
-  renderer = new WebGLRenderer( { antialias: true } );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  return state;
+};
 
-}
+const animate = (state, time) => {
+  time *= 0.001;
 
-function animate() {
+  if(state.mesh){
+    state.mesh.rotation.x = time;
+    state.mesh.rotation.y = time;
+  }
 
-  requestAnimationFrame( animate );
+  state.renderer && state.renderer.render(state.scene, state.camera);
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+  requestAnimationFrame((time) => animate(state, time));
 
-  renderer.render( scene, camera );
+};
 
-}
+const start = state => {
+  state = init(state);
+  requestAnimationFrame((time) => animate(state, time));
+};
+
+start(initialState);
